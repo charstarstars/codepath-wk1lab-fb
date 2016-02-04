20c855458c60f98f4e5b9fb623533ded30ef939d
Ariel Liu <aliu@lyft.com>
2016-02-03 17:46:42 -0800

    part 5 done

---
+++ codepath_wk1lab_fb/ComposeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ComposeViewController.swift
+//  codepath_wk1lab_fb
+//
+//  Created by Ariel Liu on 2/3/16.
+//  Copyright © 2016 Ariel Liu. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+    @IBAction func cancelTapped(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
