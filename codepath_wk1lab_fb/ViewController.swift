5d5175d294b67039380aa32858a08a00dba0b15b
Ariel Liu <aliu@lyft.com>
2016-02-03 16:10:27 -0800

    codepath wk1 lab

---
+++ codepath_wk1lab_fb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  codepath_wk1lab_fb
+//
+//  Created by Ariel Liu on 2/3/16.
+//  Copyright © 2016 Ariel Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
